5de96d49b9c7da82c99332fda2b01bd6da161e1e
Panda (パンダ・ベンチャーズ CEO) <panda@akplabo.io>
2026-03-05 12:47:39 -0800

    fix: json_safe._sanitizeをdumps前に必ず適用する形に書き直し

--- json_safe.py
+++ json_safe.py
@@ -2,41 +2,33 @@
 import json as _json
 import math
 
-class SafeEncoder(_json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, float):
-            if math.isnan(obj) or math.isinf(obj):
-                return None
-            return super().default(obj)
-        return super().default(obj)
-    
-    def iterencode(self, obj, _one_shot=False):
-        return super().iterencode(self._sanitize(obj), _one_shot)
-    
-    def _sanitize(self, obj):
-        if isinstance(obj, bool):
-            return int(obj)
-        if isinstance(obj, float):
-            if math.isnan(obj) or math.isinf(obj):
-                return None
-            return obj
-        if isinstance(obj, dict):
-            return {k: self._sanitize(v) for k, v in obj.items()}
-        if isinstance(obj, list):
-            return [self._sanitize(i) for i in obj]
+
+def _sanitize(obj):
+    """再帰的にbool/NaN/Infを変換"""
+    if isinstance(obj, bool):
+        return int(obj)
+    if isinstance(obj, float):
+        if math.isnan(obj) or math.isinf(obj):
+            return None
         return obj
+    if isinstance(obj, dict):
+        return {k: _sanitize(v) for k, v in obj.items()}
+    if isinstance(obj, list):
+        return [_sanitize(i) for i in obj]
+    return obj
+
 
 def dumps(obj, **kwargs):
-    kwargs.setdefault('cls', SafeEncoder)
-    kwargs.setdefault('ensure_ascii', False)
-    return _json.dumps(obj, **kwargs)
+    return _json.dumps(_sanitize(obj), **kwargs)
+
 
 def dump(obj, fp, **kwargs):
     fp.write(dumps(obj, **kwargs))
 
-# read ops はオリジナルjsonに委譲
+
 def loads(s, **kwargs):
     return _json.loads(s, **kwargs)
+
 
 def load(fp, **kwargs):
     return _json.load(fp, **kwargs)
